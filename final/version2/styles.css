afb70649a0f226cf3f7c8e80c3d9481776f19e41
glendadrew <gadrew@ucdavis.edu>
2026-01-06 09:13:49 -0800

    init

---
+++ final/version2/styles.css
@@ -0,0 +1,397 @@
+* {
+    box-sizing: border-box;
+}
+
+body {
+    font-family: Roboto, sans-serif;
+    color: rgb(235, 235, 235);
+    background-color: #333;
+    background-image: url("images/swirl-bg-dk.png");
+}
+
+div#container {
+    width: 100%;
+    max-width: 1200px;
+    /* make sure the column not too small and text stays readable */
+    min-width: 800px;
+    margin: .25em auto;
+    padding-top: .5em;
+     /* turn on to show the flexibility of the page before adding media queries */
+    /* border: 1px solid lightgray; */
+}
+
+header {
+    margin: 0 auto 2em auto;
+}
+
+#banner {
+    display: flex;
+    width: 1100px;
+    height: 250px;
+    margin: auto;
+    background-color: white;
+}
+
+#banner div {
+    opacity: .75;
+    transition: all 2s;
+}
+
+#banner div:hover {
+    width: 80%;
+    opacity: 1;
+}
+
+h1 {
+    margin: .5em;
+    font-size: 2.5em;
+    font-family: Helvetica, sans-serif;
+    letter-spacing: -.2em;
+}
+
+h2 {
+    margin: -1em 0 1em 3em;
+    font-size: 1em;
+    font-weight: normal;
+    font-style: italic;
+}
+
+main {
+    display: flex;
+    justify-content: space-evenly;
+    margin: 1em auto;
+}
+
+main section {
+    width: 33%;
+    border: 1px lightgray dashed;
+    margin: 0 1em;
+    padding: 1em;
+}
+
+
+/* section section:nth-child(2) {
+    margin: 0 1em;
+} */
+
+h3 {
+    padding-left: .5em;
+    margin-bottom: 0;
+    font-size: 1em;
+    font-weight: normal;
+}
+
+a {
+    text-decoration: none;
+}
+
+ol,
+ul {
+    list-style-type: none;
+    margin-left: 0;
+    padding-left: 0;
+}
+
+section li {
+    margin-left: 0;
+    padding-left: 1.2em;
+}
+
+section nav li a {
+    line-height: 1em;
+    display: inline-block;
+    /* turn this off and then turn on padding for hover to see why padding needs to be set in a not a:hover */
+    padding: .5em;
+    color: #f06;
+}
+
+section a:hover {
+    color: #333;
+    /* this demonstrates how the spacing gets wonky if changing text size on hover */
+    /* font-size: 22px; */
+    /* padding: .5em; */
+}
+
+#projects a:hover {
+    background-color: yellow;
+}
+
+#rd a:hover {
+    background-color: greenyellow;
+}
+
+#final a:hover {
+    background-color: turquoise;
+}
+
+footer {
+    display: flex;
+    justify-content: center;
+    margin-top: 2em;
+    padding: 1em;
+    border-top: 5px greenyellow dotted;
+    /* background: linear-gradient(to bottom right, yellow, greenyellow); */
+}
+
+footer nav {
+    /* padding: 1em 0; */
+}
+
+footer li {
+    display: inline-block;
+    font-weight: bold;
+    font-size: .9em;
+    /* text-align: center; */
+}
+
+footer a {
+    padding: 1.5em;
+    color: yellow
+}
+
+footer a:hover {
+    background-color: #f06;
+    color: white;
+}
+
+
+/* tried using flex:n instead of width:npx but the movement was jittery w flex */
+
+#st1 {
+    width: 55px;
+    background-color: rgb(182, 12, 182);
+}
+
+#st1:hover,
+#st7:hover,
+#st11:hover,
+#st13:hover,
+#st19:hover,
+#st20:hover {
+    background-color: black;
+    cursor: cell;
+}
+
+#st2 {
+    width: 25px;
+    background-color: lawngreen;
+}
+
+#st2:hover {
+    cursor: ne-resize;
+}
+
+#st3 {
+    width: 35px;
+    background-color: rgb(238, 255, 82);
+    animation-duration: 5s;
+}
+
+#st3:hover {
+    cursor: nw-resize;
+}
+
+#st3,
+#question {
+    animation-name: fade;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+}
+
+#st4,
+#st22 {
+    width: 35px;
+    background-color: rgb(252, 113, 0);
+}
+
+#st4:hover,
+#st22:hover {
+    cursor: se-resize;
+}
+
+#st5,
+st#23 {
+    width: 77px;
+    background-color: rgb(231, 31, 75);
+}
+
+#st5:hover,
+#st23:hover {
+    cursor: sw-resize;
+}
+
+#st6 {
+    width: 55px;
+    background-color: rgb(0, 214, 252);
+}
+
+#st6:hover {
+    cursor: ne-resize;
+}
+
+#st7 {
+    width: 25px;
+    background-color: rgb(134, 45, 57);
+}
+
+#st8 {
+    width: 25px;
+    background-color: rgb(252, 248, 0);
+}
+
+#st8:hover {
+    cursor: nw-resize;
+}
+
+#st9,
+#st24 {
+    width: 35px;
+    background-color: rgb(21, 184, 248);
+}
+
+#st9:hover,
+#st24:hover {
+    cursor: se-resize;
+}
+
+#st10 {
+    width: 35px;
+    background-color: rgb(252, 113, 0);
+}
+
+#st10:hover {
+    cursor: sw-resize;
+}
+
+#st11,
+#st25 {
+    width: 77px;
+    background-color: rgb(224, 31, 231);
+}
+
+#st12,
+#st25:hover {
+    width: 35px;
+    background-color: turquoise
+}
+
+#st12:hover {
+    cursor: ne-resize;
+}
+
+#st13 {
+    width: 25px;
+    background-color: rgb(150, 6, 150);
+}
+
+#st14 {
+    width: 45px;
+    background-color: lawngreen;
+}
+
+#st14:hover {
+    cursor: nw-resize;
+}
+
+#st15 {
+    width: 15px;
+    background-color: rgb(238, 255, 82);
+}
+
+#st15:hover {
+    cursor: se-resize;
+}
+
+#st16 {
+    width: 35px;
+    background-color: rgb(27, 253, 148);
+}
+
+#st16:hover {
+    cursor: sw-resize;
+}
+
+#st17,
+#st27 {
+    width: 77px;
+    background-color: rgb(31, 84, 231);
+}
+
+#st17:hover,
+#st27:hover {
+    cursor: ne-resize;
+}
+
+#st18 {
+    width: 45px;
+    background-color: rgb(0, 214, 252);
+}
+
+#st18:hover {
+    cursor: nw-resize;
+}
+
+#st19 {
+    width: 35px;
+    background-color: rgb(252, 0, 176);
+}
+
+#st20 {
+    width: 77px;
+    background-color: rgb(158, 106, 243);
+}
+
+#st21,
+#st26 {
+    width: 75px;
+    background-color: rgb(14, 168, 153);
+}
+
+#st21:hover,
+#st26:hover {
+    cursor: sw-resize;
+}
+
+#question {
+    animation-duration: 1s;
+}
+
+.moveDown {
+    position: relative;
+    top: 4px;
+}
+
+.moveUp {
+    position: relative;
+    bottom: 3px;
+}
+
+.space {
+    margin-left: .25em;
+}
+
+.btm-margin {
+    margin-bottom: 2em;
+}
+
+@keyframes fade {
+    0% {
+        opacity: .5;
+    }
+    25% {
+        opacity: 1;
+    }
+    50% {
+        opacity: .75;
+    }
+    75% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+}
+
+@keyframes nofade {
+    opacity: 1;
+}
